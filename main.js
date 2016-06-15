3505616916d1e865b9dd27f1ccd891b80c18cb05
K Taylor Britton <ktaylorbritton@Ks-MacBook-Pro.local>
2016-06-15 14:36:26 -0700

    used a JS fcn. to build the board and cards

--- main.js
+++ main.js
@@ -3,8 +3,22 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+var newCard;
+
+var createBoard = function(x) {
+	for (var i = 0; i < x; i++) {
+		newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+};
+
+createBoard(4);
